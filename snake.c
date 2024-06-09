7ee7b43786e6fc3cf15a52eb90174f232421cd16
Earl James Williams Aliñgasa <105076909+NeoTSZ@users.noreply.github.com>
2024-06-10 07:53:39 +0800

    Add files via upload

--- snake.c
+++ snake.c
@@ -1,185 +1,382 @@
-
-// Earl james Williams Aliñgasa (NeoTSZ on Github)
-
-// Inspiration comes from this video: 
-// https://www.youtube.com/watch?v=cUJE10XEjrU
+// Developed by Earl James Williams Aliñgasa (NeoTSZ on GitHub).
 
 /*============================================================*/
 
 #include <stdio.h>
 #include <stdlib.h>
-#include <unistd.h>
 #include <conio.h>
 #include <time.h>
 
 /*============================================================*/
 
-int side_length = 9;
+// Size macros
+#define GRID_WIDTH          11
+#define START_SNAKE_SIZE    3
+#define CELL_COUNT          (GRID_WIDTH * GRID_WIDTH)
+#define LINE_CHAR_COUNT     ((GRID_WIDTH * 2) + 2)
+#define VERTICAL_JUMP       (CELL_COUNT - GRID_WIDTH)
+#define HORIZONTAL_JUMP     (GRID_WIDTH - 1)
 
 /*============================================================*/
 
-void renderGrid(int cells[]);
-void updateSnake(int cells[], int snake[], int velocity, int size);
+// Setup macros
+#define CSI(X)              "\x1B[" #X      // Control Sequence Introducer
+#define FCS(X)              CSI(38;5;) #X   // Foreground Color Setter
 
 /*============================================================*/
 
-int main() {
-  int totalCellCount = side_length * side_length,
-  cells[totalCellCount],
-  quit = 0,
-  velocity = 0,
-  snake[totalCellCount],
-  size = 3,
-  food;
+// Color macros
+#define RESET_COLOR         CSI(0m)
+#define GREY                FCS(8m)
+#define RED                 FCS(9m)
+#define GREEN               FCS(10m)
+#define YELLOW              FCS(11m)
+#define BLUE                FCS(12m)
+#define CYAN                FCS(14m)
 
-  srand(time(0));
+/*============================================================*/
 
-  // Initializing cells
-  for (int i = 0; i < totalCellCount; i++) cells[i] = 0;
+// Cursor macros
+#define GO_UP(X)            CSI(X) "A"
+#define GO_DOWN(X)          CSI(X) "B"
+#define GO_RIGHT(X)         CSI(X) "C"
+#define GO_LEFT(X)          CSI(X) "D"
+#define RESET_CURSOR        CSI(2;3H)
+#define SAVE_CURSOR         CSI(s)
+#define LOAD_CURSOR         CSI(u)
 
-  // Initializing snake
-  for (int i = 0; i < totalCellCount; i++) snake[i] = totalCellCount / 2;
-  for (int i = 0; i < size; i++) {
-    snake[i] = (i * side_length) + (totalCellCount / 2);
-    cells[snake[i]] = 2;
-  }
-  cells[snake[0]] = 1;
+/*============================================================*/
 
-  // Initializing food
-  resetFood1:
-  food = rand() % totalCellCount;
-  for (int i = 0; i < size; i++) {
-    if (food == snake[i]) goto resetFood1;
-  }
-  cells[food] = 3;
+// Printing macros
+#define CLEAR_SCREEN        CSI(2J) CSI(H)
+#define HIDE_CURSOR         CSI(?25l)
+#define SHOW_CURSOR         CSI(?25h)
+#define DRAW_CELL           GREY "\xFA" GO_LEFT(1)
+#define DRAW_FOOD           RED "@" GO_LEFT(1)
+#define DRAW_BODY           GREEN "\xFE" GO_LEFT(1)
+#define DRAW_HEAD           YELLOW "\xFE" GO_LEFT(1)
+#define DRAW_DEAD_PART      BLUE "\xFE" GO_LEFT(1)
+#define SHOW_BUTTON(X,Y) CYAN "[" #X "]\xFF" RESET_COLOR #Y
 
-  printf("\33[?25l");
-  renderGrid(cells);
+/*============================================================*/
 
-  while (!quit) {
-    if (_kbhit()) {
-      switch (getch()) {
-        case '1': // Left
-          if (velocity != 3) velocity = 1;
-          break;
-        case '2': // Down
-          if (velocity != 5) velocity = 2;
-          break;
-        case '3': // Right
-          if (velocity != 1) velocity = 3;
-          break;
-        case '5': // Up
-          if (velocity != 2) velocity = 5;
-          break;
-      }
+typedef struct GameState
+{
+    int cells[CELL_COUNT];  // Holds the value at each cell
+    int snake[CELL_COUNT];  // Holds the positions of each snake body part
+    int length;             // Length of the snake
+    int food;               // Position of the food
+    int direction;          // Direction in which the snake moves
+    struct timespec timer;  // Used to control frame times
+} GameState;
+
+/*============================================================*/
+
+void draw_grid()
+{
+    char borderLine[LINE_CHAR_COUNT];
+    char cellDots[LINE_CHAR_COUNT];
+    int arrayIndex;
+
+    // \xBA - left/right border
+    // \xBB - top right corner
+    // \xBC - bottom right corner
+    // \xC8 - bottom left corner
+    // \xC9 - top left corner
+    // \xCD - top/bottom border
+    // \xFA - cell dot
+    // \xFE - snake body part
+    // \xFF - non-breaking space
+
+    // Setting up the character arrays borderLine[] and cellDots[]
+    for (arrayIndex = 0; arrayIndex < (LINE_CHAR_COUNT - 1); arrayIndex ++)
+    {
+        *(borderLine + arrayIndex) = '\xCD';
+        *(cellDots + arrayIndex) = (arrayIndex % 2 == 0) ? '\xFF' : '\xFA';
+    }
+    *(borderLine + arrayIndex) = '\0';
+    *(cellDots + arrayIndex) = '\0';
+
+    // Printing the top border and saving the cursor position
+    printf(HIDE_CURSOR RESET_COLOR CLEAR_SCREEN "\xC9%s\xBB", borderLine);
+    printf("\xFF" SAVE_CURSOR "\n");
+
+    // Printing the middle of the grid
+    for (int i = 0; i < GRID_WIDTH; i ++)
+    {
+        printf("\xBA" GREY "%s" RESET_COLOR "\xBA\n", cellDots);
     }
 
-    updateSnake(cells, snake, velocity, size);
-    if (velocity) renderGrid(cells);
+    // Printing the bottom border
+    printf("\xC8%s\xBC", borderLine);
 
-    // Checking if food is eaten
-    if (food == snake[0]) {
-      resetFood2:
-      food = rand() % totalCellCount;
-      for (int i = 0; i < size; i++) {
-        if (food == snake[i]) goto resetFood2;
-      }
-      cells[food] = 3;
-      size++;
-      renderGrid(cells);
-    }
+    // Printing the controls
+    printf(RESET_COLOR LOAD_CURSOR GO_DOWN(1) SAVE_CURSOR);
+    printf("CONTROLS:" LOAD_CURSOR GO_DOWN(1) SAVE_CURSOR);
+    printf(SHOW_BUTTON(W/5, Up) LOAD_CURSOR GO_DOWN(1) SAVE_CURSOR);
+    printf(SHOW_BUTTON(S/2, Down) LOAD_CURSOR GO_DOWN(1) SAVE_CURSOR);
+    printf(SHOW_BUTTON(D/3, Right) LOAD_CURSOR GO_DOWN(1) SAVE_CURSOR);
+    printf(SHOW_BUTTON(A/1, Left) LOAD_CURSOR GO_DOWN(1) SAVE_CURSOR);
+    printf(SHOW_BUTTON(E, Exit) RESET_CURSOR);
 
-    // Checking if snake eats itself
-    for (int i = 1; i < size; i++) {
-      if (snake[0] == snake[i]) {
-        quit = 1;
-        cells[snake[0]] = 4;
-        renderGrid(cells);
-        break;
-      }
-    }
-    usleep(100000);
-  }
-
-  printf("\n\nFinal score: %d\n\n", size - 3);
-
-  return 0;
+    return;
 }
 
 /*============================================================*/
 
-void renderGrid(int cells[]) {
-  int tracker = 0,
-  temp;
+void draw_at_position(int cellValue, int position)
+{
+    // Getting the row and column associated with the position on the grid
+    int row = position / GRID_WIDTH;
+    int column = position % GRID_WIDTH;
 
-  system("@cls||clear");
-  printf("\33[?25l");
-
-  // Printing first line
-  printf("%c", 201);
-  for (int i = 0; i < side_length * 2; i++) printf("%c", 205);
-  printf("%c\n", 187);
-
-  // Printing middle lines
-  for (int i = 0; i < side_length; i++) {
-    printf("%c", 186);
-    for (int j = 0; j < side_length; j++) {
-      temp = cells[tracker];
-
-      if (temp == 1) printf("%c%c", 219, 219); // Head
-      else if (temp == 2) printf("%c%c", 177, 177); // Body
-      else if (temp == 3) printf("=%c", 254); // Food
-      else if (temp == 4) printf("[]");  // Collide
-      else printf("%c%c", 255, 255); // Empty
-
-      tracker++;
+    // Moving the cursor to the position
+    printf(RESET_CURSOR);
+    for (int i = 0; i < row; i ++)
+    {
+        printf(GO_DOWN(1));
     }
-    printf("%c\n", 186);
-  }
+    for (int i = 0; i < column; i ++)
+    {
+        printf(GO_RIGHT(2));
+    }
 
-  // Printing last line
-  printf("%c", 200);
-  for (int i = 0; i < side_length * 2; i++) printf("%c", 205);
-  printf("%c", 188);
+    // Printing the character associated with cellValue
+    switch (cellValue)
+    {
+    case 0: // Empty cell
+        printf(DRAW_CELL);
+        break;
 
-  return;
+    case 1: // Snake head
+        printf(DRAW_HEAD);
+        break;
+
+    case 2: // Snake body part
+        printf(DRAW_BODY);
+        break;
+
+    case 3: // Food piece
+        printf(DRAW_FOOD);
+        break;
+
+    case 4: // Dead snake body part
+        printf(DRAW_DEAD_PART);
+        break;
+    }
+
+    return;
 }
 
 /*============================================================*/
 
-void updateSnake(int cells[], int snake[], int velocity, int size) {
-  int head = snake[0];
+void put_food(GameState* game)
+{
+    int foodPosition;
 
-  // Updating only if there is non-zero velocity
-  if (velocity) {
-    cells[snake[size - 1]] = 0;
+    // Randomly choosing cells until an empty cell is found
+    while (1)
+    {
+        foodPosition = rand() % 81;
+        if (*(game->cells + foodPosition) == 0)
+        {
+            *(game->cells + foodPosition) = 3;
+            game->food = foodPosition;
+            draw_at_position(3, foodPosition);
 
-    // Updating body
-    for (int i = size - 1; i > 0; i--) snake[i] = snake[i - 1];
+            return;
+        }
+    }
+}
 
-    switch (velocity) {
-      case 1:   // Left
-        if (head % side_length == 0) head += (side_length - 1);
-        else head--;
+/*============================================================*/
+
+void initialize_game(GameState* game)
+{
+    draw_grid();
+
+    // Setting up the randomizer and timer
+    srand(time(0));
+    game->timer.tv_sec = 0;
+    game->timer.tv_nsec = 100000000;
+
+    // Initializing the cells
+    for (int i = 0; i < CELL_COUNT; i ++)
+    {
+        *(game->cells + i) = 0;
+    }
+
+    // Initializing the snake
+    game->length = START_SNAKE_SIZE;
+    game->direction = 0;
+    for (int i = 0; i < START_SNAKE_SIZE; i ++)
+    {
+        int tempPosition = (CELL_COUNT / 2) + (i * GRID_WIDTH);
+        *(game->snake + i) = tempPosition;
+        *(game->cells + tempPosition) = (i == 0) ? 1 : 2;
+    }
+
+    // Putting the snake on the grid
+    draw_at_position(1, *(game->snake));
+    for (int i = 1; i < START_SNAKE_SIZE; i ++)
+    {
+        draw_at_position(2, *(game->snake + i));
+    }
+
+    // Putting the food on the grid
+    put_food(game);
+
+    return;
+}
+
+/*============================================================*/
+
+void play_death_animation(GameState* game)
+{
+    // Making the snake blue
+    game->timer.tv_nsec = 50000000;
+    for (int i = 0; i < game->length; i ++)
+    {
+        draw_at_position(4, *(game->snake + i));
+        nanosleep(&(game->timer), NULL);
+    }
+
+    // Showing outro message
+    printf(RESET_COLOR RESET_CURSOR);
+    for (int i = 0; i <= GRID_WIDTH; i ++)
+    {
+        printf(GO_DOWN(1));
+    }
+    printf("\nYour score is " GREEN "%d" RESET_COLOR "!\n", (game->length - 3));
+
+    // Prompting the user to close the terminal
+    printf("\nPress ENTER to close the game.\n");
+    fflush(stdin);
+    getchar();
+
+    exit(0);
+}
+
+/*============================================================*/
+
+void process_input(char input, GameState* game)
+{
+    switch (input)
+    {
+    case 'w': case 'W': case '5':   // Move up
+        game->direction = (game->direction != 2) ? 5 : game->direction;
         break;
-      case 2:   // Down
-        if (head / side_length == (side_length - 1)) head -= side_length * (side_length - 1);
-        else head += side_length;
+
+    case 's': case 'S': case '2':   // Move down
+        game->direction = (game->direction != 5) ? 2 : game->direction;
         break;
-      case 3:   // Right
-        if (head % side_length == (side_length - 1)) head -= (side_length - 1);
-        else head++;
+
+    case 'd': case 'D': case '3':   // Move right
+        game->direction = (game->direction != 1) ? 3 : game->direction;
         break;
-      default:  // Up
-        if (head < side_length) head += side_length * (side_length - 1);
-        else head -= side_length;
+
+    case 'a': case 'A': case '1':   // Move left
+        game->direction = (game->direction != 3) ? 1 : game->direction;
+        break;
+
+    case 'e': case 'E':             // Exit the game
+        play_death_animation(game);
+    }
+
+    return;
+}
+
+/*============================================================*/
+
+void update_snake(GameState* game)
+{
+    int head = *(game->snake);
+    int tail = *(game->snake + game->length - 1);
+
+    // Removing the head and tail
+    *(game->cells + head) = 2;
+    *(game->cells + tail) = 0;
+    draw_at_position(2, head);
+    draw_at_position(0, tail);
+
+    // Updating the body
+    for (int i = (game->length - 1); i > 0; i --)
+    {
+        *(game->snake + i) = *(game->snake + i - 1);
+    }
+
+    // Updating the head's location
+    switch (game->direction)
+    {
+    case 5: // Move up
+        head = (head < GRID_WIDTH) ? head + VERTICAL_JUMP : head - GRID_WIDTH;
+        break;
+
+    case 2: // Move down
+        head = (head < VERTICAL_JUMP) ? head + GRID_WIDTH : head - VERTICAL_JUMP;
+        break;
+
+    case 3: // Move right
+        head = (head % GRID_WIDTH == HORIZONTAL_JUMP) ? head - HORIZONTAL_JUMP : head + 1;
+        break;
+
+    case 1: // Move left
+        head = (head % GRID_WIDTH == 0) ? head + HORIZONTAL_JUMP : head - 1;
         break;
     }
 
-    snake[0] = head;
-    cells[head] = 1;
-    cells[snake[1]] = 2;
-  }
+    // Saving the current value of the cell before overriding it
+    int valueAtHeadCell = *(game->cells + head);
 
-  return;
+    // Printing the head onto the grid
+    *(game->snake) = head;
+    *(game->cells + head) = 1;
+    draw_at_position(1, head);
+
+    // Checking if the food was eaten
+    if (head == game->food)
+    {
+        game->length += 1;
+        *(game->snake + game->length - 1) = tail;
+        *(game->cells + tail) = 1;
+        put_food(game);
+        draw_at_position(2, tail);
+    }
+
+    // Checking if the snake eats itself
+    if (valueAtHeadCell == 2)
+    {
+        process_input('e', game);
+    }
+
+    nanosleep(&(game->timer), NULL);
+
+    return;
+}
+
+/*============================================================*/
+
+int main(void)
+{
+    GameState game;
+
+    initialize_game(&game);
+
+    // Looping the game
+    while (1)
+    {
+        // Checking for user input
+        if (_kbhit())
+        {
+            process_input(getch(), &game);
+        }
+
+        // Updating the snake's body on the grid if it is moving
+        if (game.direction)
+        {
+            update_snake(&game);
+        }
+    }
+
+    return 0;
 }
